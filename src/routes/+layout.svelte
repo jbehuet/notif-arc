2b871e38c23b478d315faab2595c945dac46bd11
Jérôme BEHUET <jbehuet@gmail.com>
2025-10-22 12:51:19 +0200

    SEO + OG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,14 @@
 <header class="container">
     <nav>
         <ul><li class="logo"><a href="/" class="item-center"><img src="/notif-arc-logo-512.png" width="68" /><strong>NotifArc</strong></a>
-            <small>Toutes vos compétitions, directement par notifications.</small>
+            <small>Ne manquez plus aucune compétition.</small>
         </li></ul>
     </nav>
 </header>
 
 <main class="container">
     <slot />
-    <footer class="container">
-        <small>par <a href="mailto:jbehuet@gmail.com">Jerome B.</a> © {new Date().getFullYear()} — NotifArc v0.1.0</small>
+    <footer class="container" style="text-align: right;">
+        <small>par <a href="mailto:jbehuet@gmail.com">Jerome B.</a> © {new Date().getFullYear()} — NotifArc v0.1.1</small>
     </footer>
 </main>
